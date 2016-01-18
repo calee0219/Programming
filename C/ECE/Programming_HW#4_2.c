d14195b5568991558c0cc1f7cdb641cff62b3e96
Gavin Lee <sz110010@gmail.com>
2016-01-18 17:46:38 +0800

    Edit Programming_HW#4_2.c
    Edit C/ECE/Programming_HW#4_2.c

--- C/ECE/Programming_HW#4_2.c
+++ C/ECE/Programming_HW#4_2.c
@@ -25,13 +25,58 @@
 /* other */
 #define MAX(x,y) ((x>y)?x:y)
 
+/* this file */
+#define TOTAL_TIME 720
+
+typedef struct q {
+    int time;
+    struct q* next;
+} queue;
+
+void push_back(queue* line, int arrive);
+queue* pop_front(queue* line);
+
 int main(int argc, int* argv[])
 {
-    srand(time(NULL));
-    int Ta = argv[argc-2];
-    int Ts = argv[argc-1];
-    int arrive = rand() % Ta;
-    int service = rand() % Ts;
+//    srand(time(NULL));
+    int Ta = 4;//*argv[argc-2];
+    int Ts = 4;//*argv[argc-1];
+    int arrive, service;
+    int now = 0;
+    int last = 0;
+    queue* waiting_line;
+    arrive = rand() % Ta + 1;
+    service = rand() % Ts + 1;
+    push_back(waiting_line, arrive);
+    for(now = 0; now < TOTAL_TIME; ++now)
+    {
+        if(now == arrive + last)
+        {
+            printf("Next customer arrives!");
+            arrive = rand() % Ta + 1;
+            service = rand() % Ts + 1;
+            push_back(waiting_line, arrive);
+            last = now;
+        }
+    }
     return 0;
+}
+
+void push_back(queue* line, int arrive)
+{
+    while(line != NULL)
+        line = line->next;
+    line = malloc(sizeof(queue));
+    line->time = arrive;
+    line->next = NULL;
+    return;
+}
+
+queue* pop_front(queue* line)
+{
+    queue* tmp = line;
+    line = line->next;
+    free(tmp);
+    return line;
 }
 
