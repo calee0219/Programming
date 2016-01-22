a015034998a5696a2635bc66533d83762a283ba1
Gavin Lee <sz110010@gmail.com>
2016-01-23 03:27:03 +0800

    Add Codeforces_Round#228(Div.2) folder
    Add CP/Codeforces/Codeforces_Round#228(Div.2) folder and files inside

--- CP/Codeforces/Codeforces_Round#228(Div.2)/PD_2.cpp
+++ CP/Codeforces/Codeforces_Round#228(Div.2)/PD_2.cpp
@@ -5,21 +5,23 @@ int main()
 {
 	int k;
 	cin >> k;
-	int sq = sqrt(k);
-	int cont = 0;
-	for(int i = 0; i <= 2*sq+4; ++i)
+	int loop = (int)log(k) / log(2) + 1;
+	int node = 2 + 3*loop+1 + k-pow(2, loop)*(loop*2-1);
+	char matrix[1000][1000];
+	for(int i = 1; i <= node; ++i)
 	{
-		for(int j = 0; j <= 2*sq+4; ++j)
+		for(int j = 1; j <= node; ++j)
 		{
-			if(i == 0 && (j>= 2 && j <= sq+3)) cout << 'Y';
-			else if(i == 1 && (j >= sq+4 && j <= 2*sq+4)) cout << 'Y';
-			else if(j == 0 && (i>= 2 && i <= sq+3)) cout << 'Y';
-			else if(j == 1 && (i >= sq+4 && i <= 2*sq+4)) cout << 'Y';
-			else if(cont < k)
+			if(i == j) matrix[i][j] = 'N';
+			else if((i == 1 && j == 2) || (i == 2 && j == 1)) matrix[i][j] = 'N';
+			else if((i == 1 && j == 3) || (i == 3 && j == 1) || (i == 2 && j == 4) || (i == 4 && j == 2)) matrix[i][j] = 'Y';
+			else if(i > 4 && i < 3*loop+4 && j > 4 && j < 3*loop+4)
 			{
-
+				if(j == 2*loop+i || i == 2*loop+j) matrix = 'Y';
+				else if()
 			}
-			else
+			else matrix[i][j] = 'N';
 		}
 	}
+	return 0;
 }
