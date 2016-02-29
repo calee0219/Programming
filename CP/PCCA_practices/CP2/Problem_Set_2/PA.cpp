af8186e989e5e7ee9a15142ba4854c99104428d3
Gavin Lee <sz110010@gmail.com>
2016-02-29 23:57:25 +0800

    Move PA_2.cpp to PA_2

--- CP/PCCA_practices/CP2/Problem_Set_2/PA.cpp
+++ CP/PCCA_practices/CP2/Problem_Set_2/PA.cpp
@@ -2,48 +2,62 @@
 	> File Name: PA.cpp
 	> Author: Gavin Lee
 	> Mail: sz110010@gmail.com
-	> Created Time: 西元2016年02月24日 (週三) 02時57分39秒
+	> Created Time: 西元2016年02月24日 (週日 ) 10時57分39秒
  ************************************************************************/
 
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+bool first = true;
+void train(void);
+
 int main()
 {
-    int n;
-    cin >> n;
+    bool inside = true;
+    scanf("%d", &n);
     while(n)
     {
-        queue<int> q;
-        int m;
-        cin >> m;
-        if( m == 0)
-        {
-            cin >> n;
-            if(n)
-                cout << endl;
-            continue;
-        }
-        q.push(m);
-        for(int i = 1; i < n; ++i)
-        {
-            cin >> m;
-            q.push(m);
-        }
-        vector<int> v;
-        for(int i = 1; i <= n; ++i)
-        {
-            v.push_back(i);
-            while(!v.empty() && !q.empty() && v.back() == q.front())
-            {
-                v.pop_back();
-                q.pop();
-            }            
-        }
-        if(v.empty())
-            cout << "Yes" << endl;
-        else
-            cout << "No" << endl;
+        if(inside) inside = false;
+        else cout << endl;
+        train();
+        scanf("%d", &n);
     }
     return 0;
+}
+
+void train(void)
+{
+    while(true)
+    {
+        vector<int> v;
+        int c;
+        int ord = 1;
+        for(int i = 1; i <= n; ++i)
+        {
+            scanf("%d", &c);
+            if(c == 0) return;
+            if(!v.empty() && v.back() == c)
+            {
+                v.pop_back();
+                continue;
+            }
+            while(ord <= n)
+            {
+                v.push_back(ord);
+                ord++;
+                if(!v.empty() && v.back() == c)
+                {
+                    v.pop_back();
+                    break;
+                }
+            }
+        }
+        if(first) first = false;
+        else cout << endl;
+        if(v.empty())
+            cout << "Yes";
+        else
+            cout << "No";
+    }
 }
