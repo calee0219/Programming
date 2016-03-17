69e1be3a5b4f5ccf53ef94954580cae26cdb5290
Gavin Lee <sz110010@gmail.com>
2016-03-17 14:40:06 +0800

    Add CPE folder for CPE

---
+++ Cpp/CPE/Fibonacci_numbers.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: Fibonacci_numbers.cpp
+	> Author: Gavin Lee
+	> Mail: sz110010@gmail.com
+	> Created Time: 西元2016年03月15日 (週二) 00時09分08秒
+ ************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
---
+++ Cpp/CPE/Hello_World.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: Hello_World.cpp
+	> Author: Gavin Lee
+	> Mail: sz110010@gmail.com
+	> Created Time: 西元2016年03月14日 (週一) 23時58分48秒
+ ************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Hello World!" << endl;
+    return 0;
+}
