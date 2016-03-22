c16ee0544fea1b3f6e18a08099a58dad81ab6beb
Gavin Lee <sz110010@gmail.com>
2016-03-22 11:40:08 +0800

    Edit HW1 p1

--- Cpp/OOP/HW2/p1.cpp
+++ Cpp/OOP/HW2/p1.cpp
@@ -8,6 +8,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class matrix {
+public:
+    matrix();
+    // member function for getting every elements of matrix
+    void scan()
+    {
+        char str[100];
+        while(cin.peek() != '\n')
+        {
+            vector<int> v;
+            while(cin.peek() < '9' && cin.peek() > '0')
+            {
+                int tmp;
+                cin >> tmp;
+                v.push_back(tmp);
+            }
+            matrix::_e.push_back(v);
+            cin.getline(str,100);
+        }
+        cin.getline(str,100);
+    }
+    // member function for display the matrix
+    void print()
+    {
+        if(matrix::_e.size() == 0)
+        {
+            cout << "null matrix" << endl;
+            return;
+        }
+        for(unsigned int i = 0; i < matrix::_e.size(); ++i)
+        {
+            for(unsigned int j = 0; j < matrix::_e.at(i).size(); ++j)
+                cout << matrix::_e.at(i).at(j) << ' ';
+            cout << endl;
+        }
+        return;
+    }
+    // member function for getting matrix[x][y]
+    int at(const int &x, const int &y)
+    {
+        return matrix::_e.at(x).at(y);
+    }
+    // overloading * for matrix
+    matrix operator*(const matrix&)
+    {}
+private:
+    vector<vector<int> > _e;
+};
+
 int main()
 {
     vector<vector<int> > m1, m2;
