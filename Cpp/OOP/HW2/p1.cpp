f5388c9c2c991a1af566c4bcddd1dbbcbaf54897
Gavin Lee <sz110010@gmail.com>
2016-03-22 12:50:04 +0800

    Edit p1
    Not finish

--- Cpp/OOP/HW2/p1.cpp
+++ Cpp/OOP/HW2/p1.cpp
@@ -59,39 +59,11 @@ private:
 
 int main()
 {
-    vector<vector<int> > m1, m2;
-    char str[2];
-    while(cin.peek() != '\n')
-    {
-        vector<int> v;
-        while(cin.peek() < '9' && cin.peek() > '0')
-        {
-            int tmp;
-            cin >> tmp;
-            v.push_back(tmp);
-        }
-        m1.push_back(v);
-        cin.getline(str,2);
-    }
-    cin.getline(str,2);
-    while(cin.peek() != '\n')
-    {
-        vector<int> v;
-        while(cin.peek() != '\n')
-        {
-            int tmp;
-            cin >> tmp;
-            v.push_back(tmp);
-        }
-        m2.push_back(v);
-        char str[2];
-        cin.getline(str,2);
-    }
-    for(unsigned int i = 0; i < m1.size(); ++i)
-    {
-        for(unsigned int j = 0; j < m1.at(i).size(); ++j)
-            cout << m1.at(i).at(j) << ' ';
-        cout << endl;
-    }
+    matrix m1, m2;
+    m1.scan();
+    m2.scan();
+    matrix ans;
+    ans = m1 * m2;
+    ans.print();
     return 0;
 }
