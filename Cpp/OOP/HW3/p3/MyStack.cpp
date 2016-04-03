62a698cfeb183418a8d3976210d4899a443dfc18
Gavin Lee <sz110010@gmail.com>
2016-04-03 23:53:21 +0800

    Modify structure of this folder

---
+++ Cpp/OOP/HW3/p3/MyStack.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: MyStack.cpp
+	> Author: Gavin Lee
+	> School: National Chiao Tung University
+	> Team: NCTU_Ragnorok
+	> Mail: sz110010@gmail.com
+	> Created Time: Sun 03 Apr 2016 09:39:44 PM CST
+ ************************************************************************/
+
+#include <bits/stdc++.h>
+#include "MyStack.h"
+using namespace std;
+
+// MyStack
+MyStack::MyStack() { size=0; }
+MyStack::~MyStack() { size=0; }
+template <class T>
+void MyStack::push(T &stack) {
+    MyStack::MyStack stack = new(MyStack);
+}
+void MyStack::pop() {}
+template <class T>
+T& MyStack::top() {}
+size_t MyStack::getSize() const {}
+bool MyStack::empty() const {}
+// Node
+template <class T>
+MyStack::Node::(T &stack, Node *node) {}
