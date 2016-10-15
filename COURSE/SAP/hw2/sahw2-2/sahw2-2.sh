758b1aefc2a0674db76049efeb313e406491c82c
Gavin Lee <sz110010@gmail.com>
2016-10-16 05:50:14 +0800

    Almost finish basic function
    Still have bugs on links when the url is such as github.com
    Left shell command and front/back page havn't write

--- COURSE/SAP/hw2/sahw2-2/sahw2-2.sh
+++ COURSE/SAP/hw2/sahw2-2/sahw2-2.sh
@@ -2,113 +2,165 @@
 
 init() {
 	exec 1>&2
-	url="google.com"
+	url="nasa.cs.nctu.edu.tw"
+}
+error() {
+	dialog \
+		--title 'ERROR' \
+		--msgbox "$(cat error.dio)" 16 51
+}
+testURL() {
+	isURL=false
+	tmp=$(echo $urlTmp | sed -e 's~http[s]*://~~' -e 's~ftp://~~')
+	cnt=$(ping -c 1 "${tmp%%/*}" | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+	if [ $cnt -ne 0 ] ; then
+		isURL=true
+	fi
+}
+urlProcess() {
+	url=$(echo $url | awk ' \
+		{
+			if($0 ~ /^(https?|ftp):\/\//) print $1;
+			else print "https://"$1;
+		}
+	' | awk ' \
+		{
+			if($0 ~ /\/$/) print $1;
+			else print $1"/";
+		}
+	')
 }
 welcome() {
 	dialog \
 		--title 'dialog browser' \
-		--clear \
 		--yesno "$(cat welcome.dio)" 16 51
 }
 web() {
+	urlProcess
 	dialog \
 		--title "dialog browser for $url" \
-		--clear \
 		--msgbox "$(w3m -dump $url)" 32 102
 }
 input() {
 	dialog \
 		--title 'input' \
-		--clear \
 		--cancel-label "Exit" \
 		--inputbox "input URL or Command" 16 51 2> cmd-url
 }
 source() {
 	dialog \
 		--title 'source of the Web' \
-		--clear \
 		--msgbox "$(w3m -dump_source $url)" 32 102
 }
-menu() {
+getLink() {
 	w3m -dump_source $url > lin
-	cat lin | awk ' \
-			{
-				if
-			} \
-		'
-	while true; do
-		cnt=0
-		dialog \
-			--backtitle "Got Link" \
-			--title "Link" \
-			--clear \
-			--cancel-label "Exit" \
-			--menu "Select a link:" 0 0 8 \
-			"1" "Display System Information" \
-			"2" "Display Disk Space" \
-			"3" "Display Home Space Utilization" \
-			2> selection
-		case $? in
-			0)
-				clear
-				echo "Program terminated."
-				exit
-				;;
-			1)
-				clear
-				echo "Program aborted." >&2
-				exit 1
-				;;
-		esac
-		case $selection in
-			0 )
-				clear
-				echo "Program terminated."
-				;;
-			1 )
-				result=$(echo "Hostname: $HOSTNAME"; uptime)
-				display_result "System Information"
-				;;
-			2 )
-				result=$(df -h)
-				display_result "Disk Space"
-				;;
-			3 )
-				if [[ $(id -u) -eq 0 ]]; then
-					result=$(du -sh /home/* 2> /dev/null)
-					display_result "Home Space Utilization (All Users)"
-				else
-					result=$(du -sh $HOME 2> /dev/null)
-					display_result "Home Space Utilization ($USER)"
-				fi
-				;;
-		esac
-	done
+	# make link into linkArr array
+	cat lin | sed -e "s/</>/g" | awk ' \
+		BEGIN { RS=">"; } \
+		{
+			if($1 == "a") { print $2; }
+		}
+	' | sed -e "s/\"//g" -e "s/href=//" -e "s/^\///" | awk -v pos="$url" ' \
+		{
+			if($0 ~ /^(https?|ftp):\/\//) print $1;
+			else if(pos ~ /^(https?|ftp):\/\//) print pos$1;
+			else print "https://"pos$1;
+		}
+	' | awk ' \
+		{
+			if($0 ~ /\/$/) print $1;
+			else print $1"/";
+		}
+	' > linkTable
 }
 link() {
-	menu
+	getLink
+	dialog \
+		--backtitle "Got Link" \
+		--title "Link under $url" \
+		--scrollbar \
+		--cancel-label "Back" \
+		--menu "Select a link:" 0 0 8 \
+		$(cat linkTable | awk '{ print NR,$1; }') \
+		2> selection
+	case $? in
+		0) no=$(cat selection)
+			url=$(awk -v no="$no" 'NR==no' linkTable)
+			web;;
+		1);;
+		*) error;;
+	esac
 }
 download() {
-	menu
+	getLink
+	dialog \
+		--backtitle "Download HTML" \
+		--title "Download Source Code from $url" \
+		--scrollbar \
+		--cancel-label "Back" \
+		--menu "Select a link:" 0 0 8 \
+		$(cat linkTable | awk '{ print NR,$1; }') \
+		2> selection
+	case $? in
+		0) no=$(cat selection)
+			url=$(awk -v no="$no" 'NR==no' linkTable)
+			tmp=$(echo $url | sed -e 's~http[s]*://~~' -e 's~ftp://~~' -e 's~/~.~g')
+			wget -O ~/Downloads/${tmp%.} $url;;
+		1) ;;
+		*) error;;
+	esac
 }
 bookmark() {
+	dialog \
+		--backtitle "Book Mark" \
+		--title "Choose bookmark or add one" \
+		--scrollbar \
+		--cancel-label "Back" \
+		--menu "Select a link:" 0 0 8 \
+		"1" "Add a bookmark" \
+		"2" "Delete a bookmark" \
+		$(cat bookmark.tab | awk '{ print NR+2,$1; }') \
+		2> selection
+	case $? in
+		0) no=$(cat selection)
+			case $no in
+				1) awk -v url=$url ' \
+					BEGIN { there=0; }
+					{ if($0 == url) there=1; }
+					END { if(there == 0) print url; }
+				' bookmark.tab >> bookmark.tab;;
+				2) awk -v url="$url" ' \
+					{ if($0 !~ url) print $1; }
+				' bookmark.tab > mark
+				cat mark > bookmark.tab;;
+				*) url=$(awk -v no="$no" 'NR==(no-2)' bookmark.tab)
+					web;;
+			esac;;
+		1) ;;
+		*) error;;
+	esac
 }
 help() {
 	dialog \
 	   	--title 'help' \
-		--clear \
 		--msgbox "$(cat help.dio)" 32 102
+}
+badCondition() {
+	dialog \
+		--title "Input \"$(cat cmd-url)\" error" \
+		--msgbox "$(cat badCondition.dio)" 16 51
 }
 goodbye() {
 	dialog \
 	   	--title 'Good Bye' \
-		--clear \
 		--msgbox "$(cat bye.dio)" 16 51
 }
 end() {
 	rm cmd-url
 	rm selection
 	rm lin
+	rm linkTable
+	rm mark
 }
 
 
@@ -117,31 +169,30 @@ welcome
 if [ $? -eq 1 ] ; then
 	dialog \
 	   	--title 'Out of browser' \
-		--clear \
 		--msgbox "$(cat out.dio)" 16 51
 else
 	web
 	while true; do
 		input
 		case $? in
-			0) tmp=$(cat cmd-url | sed -e 's~http[s]*://~~' -e 's~ftp*://~~')
-				cnt=$(ping -c 1 "${tmp%%/*}" | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
-				if [ $cnt -ne 0 ] ; then
+			0) urlTmp=$(cat cmd-url)
+				testURL
+				if [ $isURL == true ] ; then
 					url=$(cat cmd-url)
 					web
 				else
 					case "$(cat cmd-url)" in
-						\/S|\/s) source;;
-						\/L|\/l) link;;
-						\/D|\/d) download;;
-						\/B|\/b) bookmark;;
-						\/H|\/h) help;;
-						\/Q|\/q) break;;
-						*);;
+						\/S|\/s|\/source)	source;;
+						\/L|\/l|\/link)		link;;
+						\/D|\/d|\/download)	download;;
+						\/B|\/b|\/bookmark)	bookmark;;
+						\/H|\/h|\/help)		help;;
+						\/Q|\/q|\/quit)		break;;
+						*)					badCondition;;
 					esac
 				fi;;
 			1) break;;
-			*) break;;
+			*) error;;
 		esac
 	done
 fi
