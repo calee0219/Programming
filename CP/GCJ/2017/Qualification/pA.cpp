8db97466cb3c40550fda831f69bd4029203f255c
Gavin Lee <sz110010@gmail.com>
2017-04-09 12:58:07 +0800

    Correct problem A

--- CP/GCJ/2017/Qualification/pA.cpp
+++ CP/GCJ/2017/Qualification/pA.cpp
@@ -12,23 +12,37 @@ int main()
         cin >> str;
         cin >> k;
         int len = str.size();
-        int pan = 0;
-        int turn = (1<<k)-1;
-        for(int i = 0; i < len; ++i) {
-            pan <<= 1;
-            if(str[i] == '-') pan |= 1;
-        }
-        int ans = 0;
+        //int pan = 0;
+        //int turn = (1<<k)-1;
+        //for(int i = 0; i < len; ++i) {
+            //pan <<= 1;
+            //if(str[i] == '-') pan |= 1;
+        //}
+        //int ans = 0;
+        //for(int i = 0; i < len-k+1; ++i) {
+            //if(pan & 1) {
+                //pan ^= turn;
+                //ans++;
+            //}
+            //pan >>= 1;
+            //if(!pan) break;
+        //}
+        //if(pan) cout << "IMPOSSIBLE" << endl;
+        //else cout << ans << endl;
+        int cnt = 0;
         for(int i = 0; i < len-k+1; ++i) {
-            if(pan & 1) {
-                pan ^= turn;
-                ans++;
+            if(str[i] == '-') {
+                cnt++;
+                for(int j = 0; j < k; ++j)
+                    str[i+j] = str[i+j]=='+'? '-':'+';
             }
-            pan >>= 1;
-            if(!pan) break;
         }
-        if(pan) cout << "IMPOSSIBLE" << endl;
-        else cout << ans << endl;
+        bool ans = true;
+        for(int i = 0; i < len; ++i) {
+            if(str[i] == '-') ans = false;
+        }
+        if(ans) cout << cnt << endl;
+        else cout << "IMPOSSIBLE" << endl;
     }
     return 0;
 }
