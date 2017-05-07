df0d60afd301a01c1b67afb33fec4835c81ac5bc
Gavin Lee <sz110010@gmail.com>
2017-05-07 20:39:20 +0800

    ALGO hw4

--- COURSE/Algo/hw4/code.cpp
+++ COURSE/Algo/hw4/code.cpp
@@ -6,28 +6,32 @@ using namespace std;
 int main()
 {
     ifstream is ("input.txt", ifstream::binary);
+    is.seekg (0, is.end);
     int length = is.tellg();
+    is.seekg (0, is.beg);
     char* buffer = new char [length];
     is.read (buffer,length);
     char* offset = buffer;
     unsigned char c;
-    for(int i = 0; i < length; ++i) cout << buffer[i];
-    //while(c = *offset++) {
-        //cout << hex << (int)c << ' ';
-        //if(c < 128) {
-            //cout << (char)c;
-        //} else if(c >= 0x90 && c <= 0x9f) { // array
-            //cout << (int)(c & 15);
-            //cout << " array";
-        //} else if(c >= 0xa0 && c <= 0xbf) { // string
-            //cout << (bitset<8>)c;
-            ////cout << (int)(c & 31);
-            //cout << " string";
-        //} else {
-            //cout << (char)c;
-        //}
-        //cout << endl;
-    //}
+    //cout << length << endl;
+    //for(int i = 0; i < length; ++i) cout << buffer[i];
+    while(c = *offset++) {
+        cout << (void *)offset << " " << hex << (int)c << ' ';
+        if(c < 128) {
+            cout << (char)c;
+        } else if(c >= 0x90 && c <= 0x9f) { // array
+            cout << (int)(c & 15);
+            cout << " array";
+        } else if(c >= 0xa0 && c <= 0xbf) { // string
+            cout << (bitset<8>)c;
+            //cout << (int)(c & 31);
+            cout << " string";
+        } else {
+            cout << (char)c;
+        }
+        cout << endl;
+    }
+    is.close();
     delete[] buffer;
     return 0;
 }
